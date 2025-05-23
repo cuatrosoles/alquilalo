d333f23e54d9118e6bf362505658c809e3a71a60
Cuatrosoles <ubifon@gmail.com>
2025-05-23 02:24:58 -0300

    012

--- frontend/user/src/App.js
+++ frontend/user/src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// frontend/user/src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import ItemDetailPage from './pages/ItemDetailPage';
+import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
+import SearchResultsPage from './pages/SearchResultsPage';
+import ProfilePage from './pages/ProfilePage';
+import AccountPage from './pages/AccountPage';
+import { AuthProvider } from './contexts/AuthContext';
+import PrivateRoute from './components/PrivateRoute';
+import UserBookings from './pages/UserBookings';
+import BookingDetails from './components/BookingDetails';
+import CreateBooking from './components/CreateBooking';
+import CreateItemPage from './pages/CreateItemPage';
+import RentalConfirmationPage from './pages/RentalConfirmationPage';
+import PaymentStatusPage from './pages/PaymentStatusPage';
+import PaymentHistoryPage from './pages/PaymentHistoryPage';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/item/:id" element={<ItemDetailPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/search" element={<SearchResultsPage />} />
+          <Route path="/perfil" element={<PrivateRoute><ProfilePage /></PrivateRoute>} />
+          <Route path="/cuenta" element={<PrivateRoute><AccountPage /></PrivateRoute>} />
+
+          <Route path="/bookings" element={<PrivateRoute><UserBookings /></PrivateRoute>} />
+          <Route path="/bookings/:id" element={<PrivateRoute><BookingDetails /></PrivateRoute>} />
+          <Route path="/items/:itemId/book" element={<PrivateRoute><CreateBooking /></PrivateRoute>} />
+          <Route path="/publicar" element={<PrivateRoute><CreateItemPage /></PrivateRoute>} />
+          <Route path="/rentals/:rentalId" element={<RentalConfirmationPage />} />
+
+          <Route path="/pago/estado" element={<PaymentStatusPage />} />
+          <Route path="/mis-pagos" element={<PrivateRoute><PaymentHistoryPage /></PrivateRoute>} />
+        </Routes>
+      </Router>
+    </AuthProvider>
   );
 }
 
