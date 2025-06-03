265a684fea881fae3427873733034658b8f84170
cuatrosoles <ubifon@gmail.com>
2025-06-03 00:37:42 -0300

    Deploy Vercel 050

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,32 +1,30 @@
 // src/config/firebase.js
-import admin from 'firebase-admin';
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
-import { getAuth } from 'firebase/auth';
-import config from './config.js';
+import admin from "firebase-admin";
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import config from "./config.js";
 
 // Inicializar Firebase Admin SDK
 if (!admin.apps.length) {
   try {
+    // Usar credenciales desde la variable de entorno
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
     admin.initializeApp({
-      credential: admin.credential.applicationDefault(),
+      credential: admin.credential.cert(serviceAccount),
       projectId: config.firebaseConfig.projectId,
+      // Añadir storageBucket si lo usas con Admin SDK
+      storageBucket: config.firebaseConfig.storageBucket,
     });
   } catch (error) {
-    console.error('Error al inicializar Firebase Admin SDK:', error);
-    if (config.firebaseConfig.apiKey) {
-      admin.initializeApp({
-        apiKey: config.firebaseConfig.apiKey,
-        authDomain: config.firebaseConfig.authDomain,
-        projectId: config.firebaseConfig.projectId,
-        storageBucket: config.firebaseConfig.storageBucket,
-        messagingSenderId: config.firebaseConfig.messagingSenderId,
-        appId: config.firebaseConfig.appId,
-        measurementId: config.firebaseConfig.measurementId,
-      });
-    } else {
-      console.error('No se encontraron credenciales de Firebase.');
-    }
+    console.error(
+      "Error al inicializar Firebase Admin SDK con variable de entorno:",
+      error
+    );
+    // Si falla la inicialización con variable de entorno, puedes intentar otras opciones o lanzar un error
+    throw new Error(
+      "Failed to initialize Firebase Admin SDK: " + error.message
+    );
   }
 }
 
