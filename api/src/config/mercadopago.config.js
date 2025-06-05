51bf8555c2f051f26c876ee5225a997682db9122
cuatrosoles <ubifon@gmail.com>
2025-06-05 13:36:56 -0300

    Cambios 063

--- api/src/config/mercadopago.config.js
+++
@@ -1,2 +0,0 @@
-let publicKey = process.env.MERCADOPAGO_PUBLIC_KEY;
-let accessToken = process.env.MERCADOPAGO_ACCESS_TOKEN;
--- api/src/server.js
+++
@@ -1,8 +0,0 @@
-// src/server.js
-import app from "./app.js";
-
-const PORT = process.env.PORT || 10000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor API de Alquilalo corriendo en el puerto ${PORT}`);
-});
