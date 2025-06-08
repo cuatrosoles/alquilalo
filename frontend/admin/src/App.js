30e3c6cf5cba6cc156fa2e18087f4db8fedb8382
cuatrosoles <ubifon@gmail.com>
2025-06-07 22:24:40 -0300

    Feat 082: Configuración para despliegue separado en Vercel

--- frontend/admin/src/App.js
+++ frontend/admin/src/App.js
@@ -7,7 +7,7 @@ import { AuthProvider } from "./contexts/AuthContext";
 function App() {
   return (
     <AuthProvider>
-      <Router basename="/admin">
+      <Router>
         <AppRoutes />
       </Router>
     </AuthProvider>
