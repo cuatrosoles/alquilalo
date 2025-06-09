be78fb6d9651f10377f1596384c3cd1f85d04cee
cuatrosoles <ubifon@gmail.com>
2025-06-09 01:00:19 -0300

    Fix 092 Vercel admin

--- frontend/admin/src/pages/AppRoutes.js
+++ frontend/admin/src/pages/AppRoutes.js
@@ -1,27 +1,43 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import PrivateRoute from '../components/PrivateRoute';
-import Items from './Items';
-import FormItem from '../components/FormItem';
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import PrivateRoute from "../components/PrivateRoute";
+import Items from "./Items";
+import FormItem from "../components/FormItem";
 
 const AppRoutes = () => {
   return (
     <Routes>
-      <Route path="/items" element={
-        <PrivateRoute>
-          <Items />
-        </PrivateRoute>
-      } />
-      <Route path="/items/new" element={
-        <PrivateRoute>
-          <FormItem isEditing={false} />
-        </PrivateRoute>
-      } />
-      <Route path="/items/:id/edit" element={
-        <PrivateRoute>
-          <FormItem isEditing={true} />
-        </PrivateRoute>
-      } />
+      {/* Ruta raíz - redirigir a /items */}
+      <Route path="/" element={<Navigate to="/items" replace />} />
+
+      {/* Rutas existentes */}
+      <Route
+        path="/items"
+        element={
+          <PrivateRoute>
+            <Items />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/items/new"
+        element={
+          <PrivateRoute>
+            <FormItem isEditing={false} />
+          </PrivateRoute>
+        }
+      />
+      <Route
+        path="/items/:id/edit"
+        element={
+          <PrivateRoute>
+            <FormItem isEditing={true} />
+          </PrivateRoute>
+        }
+      />
+
+      {/* Catch all - para rutas no encontradas */}
+      <Route path="*" element={<Navigate to="/items" replace />} />
     </Routes>
   );
 };
